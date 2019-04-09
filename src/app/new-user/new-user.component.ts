6b9af7222a489280440660f9f62ecc94bd98bb0e
MalikIcm <49428943+MalikIcm@users.noreply.github.com>
2019-04-09 15:07:51 +0200

    Update new-user.component.ts

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../models/User.model';
+
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+  initForm(){
+    this.userForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      drinkPreference: ''
+    });
+  }
+  onSubmitForm(){
+    const formValue = this.userForm.value;
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['email'],
+      formValue['drinkPreference']
+    );
+    this.userService.addUser(newUser);
+    this.router.navigate(['/users']);
   }
 
 }
